eb017bccb9e1ac74fe01dc4fe7b10117ad475b73
VeneraImamova <venus.imamova@gmail.com>
2022-08-14 20:49:37 +0700

    first task done

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введидите число (А): ");
+int A = Convert.ToInt32(Console.ReadLine()); ;
+Console.WriteLine("Введидите степень (В): ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int DegreeNumbers(int number, int degree)
+{
+    int result = 1;
+    int i = 1;
+    while (i <= degree)
+    {
+        result = result * number;
+        i++;
+    }
+    return result;
+}
+
+Console.WriteLine($"Число {A} в степени {B} равно {DegreeNumbers(A, B)}");
